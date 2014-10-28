a91c9b7a05a2c1cec4f43d31ffbb52635acfe221
Brent Schooley <schooleybrent@gmail.com>
2014-10-28 11:42:18 -0400

    Initial commit

---
+++ TwilioClientServer/Controllers/ClientController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+
+namespace TwilioClientServer.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client/Token?ClientName=alice
+        public ActionResult Token(string clientName = "alice")
+        {
+            // Create a TwilioCapability object passing in your credentials.
+            var capability = new TwilioCapability("[Your Account SID]", "[Your Auth Token]");
+
+            // Specify that this token allows receiving of incoming calls
+            capability.AllowClientIncoming(clientName);
+
+            // Replace "AP*****" with your TwiML App Sid
+            capability.AllowClientOutgoing("AP********************************");
+
+            // Return the token as text
+            return Content(capability.GenerateToken());
+        }
+
+        // /Client/CallXamarin
+        public ActionResult CallXamarin()
+        {
+            var response = new TwilioResponse();
+            response.Dial(new Client("alice"));
+            return new TwiMLResult(response);
+        }
+
+        // /Client/InitiateCall?source=5551231234&target=5554561212
+        public ActionResult InitiateCall(string source, string target)
+        {
+            var response = new TwilioResponse();
+
+            // Add a <Dial> tag that specifies the callerId attribute
+            response.Dial(target, new { callerId = source });
+
+            return new TwiMLResult(response);
+        }
+    }
+}
---
+++ TwilioClientServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwilioClientServer.MvcApplication" Language="C#" %>
